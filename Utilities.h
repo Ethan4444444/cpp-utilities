4dff51f1576961a1bd3c24e60182e5b9eaf65542
Ethan Pawl <ethanpawl42@gmail.com>
2021-08-05 07:48:36 -0500

    Update Utilities.h

--- Utilities.h
+++ Utilities.h
@@ -35,6 +35,7 @@ namespace util
 	bool stob(const std::string& s);
 
 	// TODO: change default value of eps to a machine-specific value
+	// TODO: use enable_if and is_floating_point to remove the need to overload so many times.
 	bool equal(const float a, const float b, float eps = 0.001);
 	bool equal(const float a, const double b, float eps = 0.001);
 	bool equal(const float a, const long double b, float eps = 0.001);
