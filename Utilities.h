832d917d08caf3cf34c99cbbda10dc68891acdb8
Ethan Pawl <ethanpawl42@gmail.com>
2021-11-17 18:04:35 -0600

    Update Utilities.h

--- Utilities.h
+++ Utilities.h
@@ -5,35 +5,6 @@
 
 namespace util
 {
-	/*
-	void println(char value);
-	void println(unsigned char value);
-
-	void println(short value);
-	void println(unsigned short value);
-
-	void println(int value);
-	void println(unsigned int value);
-
-	void println(long value);
-	void println(unsigned long value);
-
-	void println(long long value);
-	void println(unsigned long long value);
-
-	void println(float value);
-	void println(double value);
-	void println(long double value);
-
-	void println(bool value);
-
-	void println(const void* value);
-
-	// Any non-primitive input will be passed by reference
-	template<typename T>
-	void println(T& s);
-	*/
-
 	bool stob(const std::string& s);
 
 	// TODO: change default value of eps to a machine-specific value
